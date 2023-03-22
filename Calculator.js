be9b8869c8ca3d5a6a43b1adcef587f05b950e78
Aryan <aryan.saxena094@gmail.com>
2023-03-22 00:38:03 -0400

    commit

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,25 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(){
     console.log("Server has started on port 3000");
 });
 
 app.get("/", function(req, res){
-    res.sendFile("/Users/aryansaxena/Desktop/JavaScript Learning/Project 6/index.html");
-})
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req, res){
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1+num2;
+    res.send("The result is: "+result);
+
+});
