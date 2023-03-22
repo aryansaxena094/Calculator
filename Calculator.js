01c0ebc7feb52d374d2974c603f99e1b0ef02341
Aryan <aryan.saxena094@gmail.com>
2023-03-22 00:55:39 -0400

    commit

--- Calculator.js
+++ Calculator.js
@@ -2,9 +2,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");  // Import the ejs module
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");  // Set the view engine to ejs
 
 app.listen(3000, function(){
     console.log("Server has started on port 3000");
@@ -15,11 +17,17 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
-    var result = num1+num2;
-    res.send("The result is: "+result);
+    var multiplication_result = num1*num2;
+    var sum = num1+num2;
+    var division = num1/num2;
 
+    // Render the response HTML file with dynamic content using ejs
+    res.render("response", {
+        multiplication_result: multiplication_result,
+        sum: sum,
+        division: division
+    });
 });
